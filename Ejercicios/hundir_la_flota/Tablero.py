01d8e7e4c18d063ecfeb1417407ad37b0a29bac1
André <acarnerofernandez@danielcastelao.org>
2025-11-26 12:37:03 +0100

    hundir la flota

---
+++ Ejercicios/hundir_la_flota/Tablero.py
@@ -0,0 +1,9 @@
+class Tablero:
+    def __init__(self, tamano):
+        self.dimensiones = (tamano, tamano)
+        self.casillas = [[0]*tamano for _ in range(tamano)]
+
+
+if __name__ == "__main__":
+    tablero = Tablero(5)
+    print(tablero.casillas)
