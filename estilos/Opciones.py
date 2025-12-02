443302b8249334c608db44ccf5d63905f5c234df
André <acarnerofernandez@danielcastelao.org>
2025-12-02 14:06:31 +0100

    ejercicio

---
+++ estilos/Opciones.py
@@ -0,0 +1,23 @@
+#Preguntamos para que interactue con nuestro menu
+seleccion = int(input("Escoge un color: [1]amarillo, [2]rojo, [3]azul o [4]naranja: "))
+
+AMARILLO = 1
+ROJO = 2
+AZUL = 3
+NARANJA = 4
+
+
+if seleccion == AMARILLO:
+    print ("amarillo")
+
+
+elif seleccion == ROJO:
+    print ("rojo")
+
+
+elif seleccion == AZUL:
+    print("azul")
+
+
+elif seleccion == NARANJA:
+    print("naranja")
